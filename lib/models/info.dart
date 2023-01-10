c2625feff3a0043db37f5bf70e2747bf5aafd248
Adevikki <vikkibabs@gmail.com" "Adevikki>
2023-01-10 10:38:06 +0100

    data

---
+++ lib/models/info.dart
@@ -0,0 +1,122 @@
+const info = [
+  {
+    'name': 'Rivaan Ranawat',
+    'message': 'Hey, how are you doing?',
+    'time': '3:53 pm',
+    'profilePic':
+        'https://cdn.pixabay.com/photo/2020/02/07/12/54/emoji-4827091_960_720.png',
+  },
+  {
+    'name': 'John Doe',
+    'message': 'Hello, whats up',
+    'time': '2:25 pm',
+    'profilePic':
+        'https://cdn.pixabay.com/photo/2020/02/07/12/38/emoji-4827048_960_720.png',
+  },
+  {
+    'name': 'Naman Ranawat',
+    'message': 'Hello, I want to sleep.',
+    'time': '1:03 pm',
+    'profilePic':
+        'https://cdn.pixabay.com/photo/2016/08/29/11/55/emoticon-1628080_960_720.png',
+  },
+  {
+    'name': 'Dad',
+    'message': 'Call me, have some work',
+    'time': '12:06 pm',
+    'profilePic':
+        'https://cdn.pixabay.com/photo/2020/02/09/09/41/smiley-4832504_960_720.png',
+  },
+  {
+    'name': 'Mom',
+    'message': 'You ate food?',
+    'time': '10:00 am',
+    'profilePic':
+        'https://cdn.pixabay.com/photo/2020/02/07/12/54/emoji-4827091_960_720.png',
+  },
+  {
+    'name': 'Jurica',
+    'message': 'Yo!!!!! Long time, no see!?',
+    'time': '9:53 am',
+    'profilePic':
+        'https://cdn.pixabay.com/photo/2020/02/07/12/38/emoji-4827048_960_720.png',
+  },
+  {
+    'name': 'Albert Dera',
+    'message': 'Am I fat?',
+    'time': '7:25 am',
+    'profilePic':
+        'https://cdn.pixabay.com/photo/2020/02/09/09/41/smiley-4832504_960_720.png',
+  },
+  {
+    'name': 'Joseph',
+    'message': 'I am from International Olym...',
+    'time': '6:02 am',
+    'profilePic':
+        'https://cdn.pixabay.com/photo/2020/02/07/12/54/emoji-4827091_960_720.png',
+  },
+  {
+    'name': 'Sikandar',
+    'message': 'Lets Code!',
+    'time': '4:56 am',
+    'profilePic':
+        'https://cdn.pixabay.com/photo/2016/08/29/11/55/emoticon-1628080_960_720.png',
+  },
+  {
+    'name': 'Ian Dooley',
+    'message': 'Images by Unsplash',
+    'time': '1:00 am',
+    'profilePic':
+        'https://cdn.pixabay.com/photo/2020/02/07/12/54/emoji-4827091_960_720.png',
+  },
+];
+
+const messages = [
+  {"isMe": false, "text": "Hey What is up with you!!", "time": "10:00 am"},
+  {"isMe": true, "text": "im fine,wbu?", "time": "11:00 am"},
+  {"isMe": false, "text": "I am great man!", "time": "11:01 am"},
+  {
+    "isMe": false,
+    "text": "Just messaged cuz I had some work.",
+    "time": "11:01 am"
+  },
+  {"isMe": true, "text": "Obviously, say", "time": "11:03 am"},
+  {
+    "isMe": false,
+    "text": "haha I wanted you to check out my new channel ^^",
+    "time": "11:04 am"
+  },
+  {
+    "isMe": true,
+    "text": " Sure, what is the channel name?",
+    "time": "11:05 am"
+  },
+  {
+    "isMe": false,
+    "text": "Rivaan Ranawat",
+    "time": "11:06 am",
+  },
+  {
+    "isMe": true,
+    "text": "Looks great to me!",
+    "time": "11:15 am",
+  },
+  {"isMe": false, "text": "Thanks bro!", "time": "11:17 am"},
+  {"isMe": false, "text": "Did you subscribe?", "time": "11:16 am"},
+  {"isMe": true, "text": "Yes, surely bro!", "time": "11:17 am"},
+  {
+    "isMe": false,
+    "text": "Cool, did you like the content?",
+    "time": "11:18 am",
+  },
+  {
+    "isMe": true,
+    "text": "I loved it?",
+    "time": "11:19 am",
+  },
+  {
+    "isMe": false,
+    "text": "OMG! Woah! Thanks!",
+    "time": "11:20 am",
+  },
+];
